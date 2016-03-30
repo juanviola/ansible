e521962845f5f8cedd44e90ce4edea3b0b22cd2b
Maverick <Juan Viola>
2016-03-30 17:28:09 -0300

    uploading changes for cakephp dev server

--- cakephp-development-server/roles/cakephp/templates/app.php
+++ cakephp-development-server/roles/cakephp/templates/app.php
@@ -225,8 +225,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'app_dev',
+            'password' => 'app_dev',
             'database' => 'app_dev',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
