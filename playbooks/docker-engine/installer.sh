10d2b6231bcb12873aa6750285b92815d9fa7650
Juan Viola <juan.viola@mercadolibre.com>
2017-01-10 18:08:04 -0300

    update data

--- playbooks/docker-engine/installer.sh
+++ playbooks/docker-engine/installer.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo 'localhost ansible_connection=local' >> /etc/ansible/hosts
+test -d /etc/ansible && test -f /etc/ansible/hosts \
+ || mkdir -p /etc/ansible && touch /etc/ansible/hosts
+
+test `grep -c 'localhost ansible_connection=local' /etc/ansible/hosts` == 0 && \
+ echo 'localhost ansible_connection=local' >> /etc/ansible/hosts
+
+ansible-playbook ./install-docker-engine.yml
